ebb2142051848027c7ba366525c94514f3770828
David Chiu <david.chiu0073@gmail.com>
2020-02-21 16:42:23 -0800

    wow

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,6 @@
  * GET home page.
  */
 
-//exports.view = function(req, res){
-//  res.render('index');
-// }
-
-var data = require('../users.json');
-
 exports.view = function(req, res){
-	for (i=0; i < data.users.length; i++) {
-		if (data.users[i].isUser == true) {
-			var user = data.users[i];
-			res.render('sip', user);
-			break;
-		} 
-	}
-	res.render('sip');
-};
+  res.render('index');
+}
